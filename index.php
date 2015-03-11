a970d9fa32092442eb031e3df23844feaf1277ad
alexringo <alexa.ringo@gmail.com>
2015-03-11 14:07:42 +0700

    test

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="css/bootstrap.css" type="text/css" > 
+ <link rel="stylesheet" href="css/bootstrap-responsive.css" type="text/css" >
+
+</head>
+
+<body>
+<div class="container">
+
+ <div class="navbar navbar-inner navbar-fixed-top">
+ <div class="container">
+ <ul class="nav">
+ <li class="active"><a href="#">Home</a></li>
+ <li><a href="#">Projects</a></li>
+ </ul>
+ <form class="navbar-form pull-right" role="search">
+        <div class="form-search">
+          <input type="text" class="form-control" placeholder="Search">
+        <button type="submit" class="btn btn-default">Search</button>
+        </div>
+      </form>
+</div>
+</div>
+<div class="hero-unit">
+ <h1>Praktek Dokter</h1>
+ <p>Temukan tempat praktek dokter terdekat</p>
+ <a href="#" class="btn btn-large btn-success">Get Started</a>
+</div>
+<div class="row">
+ <div class="span4">
+ <p>Dummy Text</p>
+ </div>
+ <div class="span8">
+ <p>Dummy Text</p>
+ </div>
+</div>
+</div>
+
+ <script src="js/jquery-1.11.2.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
